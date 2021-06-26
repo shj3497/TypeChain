150624bc8a113055f6c5526891afdc4b7454f18c
shj3497 <shj3497@gmail.com>
2021-06-26 17:40:08 +0900

    Validating Block Structure part Two

--- src/index.ts
+++ src/index.ts
@@ -70,6 +70,8 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 }
 
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
   if(!Block.validateStructure(candidateBlock)){
     return false;
@@ -77,8 +79,17 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
     return false;
   } else if(previousBlock.hash !== candidateBlock.previousHash){
     return false;
+  } else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){
+    return false;
+  } else {
+    return true;
   }
 }
 
+const addBlock = (candidateBlock: Block): void => {
+  if(isBlockValid(candidateBlock, getLatestBlock())){
+    blockchain.push(candidateBlock)
+  }
+}
 
 export {};
